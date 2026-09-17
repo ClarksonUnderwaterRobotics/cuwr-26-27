6817de5eeb3c1273f4bacac86732c0d737ba5c7a
AGOODFROG <caden.j.quirion@gmail.com>
2026-09-17 07:50:07 -0400

    sample program works for some reason

--- main.py
+++ main.py
@@ -22,36 +22,33 @@ def getColours(cls_num):
     random.seed(cls_num)
     return tuple(random.randint(0, 255) for _ in range(3))
 
-def drawBox():
-    x1, y1, x2, y2 = map(int, box.xyxy[0])
-
-    cls = int(box.cls[0])
-    class_name = class_names[cls]
-
-    conf = float(box.conf[0])
-
-    colour = getColours(cls)
-
-    cv2.rectangle(frame, (x1, y1), (x2, y2), colour, 2)
-
-    cv2.putText(frame, f"{class_name} {conf:.2f}",
-                (x1, max(y1 - 10, 20)), cv2.FONT_HERSHEY_SIMPLEX,
-                0.6, colour, 2)
-
 while True:
+    
     ret, frame = videoCap.read()
     if not ret:
         break
-    results = yolo.track(frame, stream=True)
-
+    results = yolo.track(frame, stream=True) 
     for result in results:
         class_names = result.names
         for box in result.boxes:
             if box.conf[0] > 0.4:
-                threading.Thread(target=drawBox).start
-                
-                            
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+
+                cls = int(box.cls[0])
+                class_name = class_names[cls]
+
+                conf = float(box.conf[0])
+
+                colour = getColours(cls)
+
+                cv2.rectangle(frame, (x1, y1), (x2, y2), colour, 2)
+
+                cv2.putText(frame, f"{class_name} {conf:.2f}",
+                            (x1, max(y1 - 10, 20)), cv2.FONT_HERSHEY_SIMPLEX,
+                            0.6, colour, 2)
+                  
     cv2.imshow("test",frame)
+    #no idea what this dose
     key = cv2.waitKey(1)
     if(key < -1):
           cv2.destroyAllWindows()
@@ -59,7 +56,7 @@ while True:
     
     #todo add exit
     frame_count += 1
-
+   
 videoCap.release()
 
 
