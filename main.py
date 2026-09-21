459c6f7511c2453017a62f07b782548dc92b2c71
AGOODFROG <caden.j.quirion@gmail.com>
2026-09-21 11:19:36 -0400

    added comments

--- main.py
+++ main.py
@@ -14,8 +14,10 @@ yolo = YOLO("yolov8s.pt")
 # random.seed(cls_num): makes color consistent across frames.
 
 # for testing run 
-# ffmpeg -f v4l2 -video_size 640x480 -i /dev/video0 -vf "format=yuv420p" -f h264 "udp://127.0.0.1:1234"^C and then run the program
-# This is lagging on my end I do not think my loopback was made for this sort of work. and my IGPU sucks at video encoding
+# ffmpeg -f v4l2 -video_size 640x480 -i /dev/video0 -vf "format=yuv420p" -f h264 "udp://127.0.0.1:1234" and then run the program
+# this may need to be ran as root. It may be SE linux or some odd quirck of the Red Hat world for me but who knows.
+# This is lagging on my end I do not think my loopback was made for this sort of work, and my IGPU sucks at video encoding. 
+# also my I have a 12th gen 
 video_path = "udp://127.0.0.1:1234"
 videoCap = cv2.VideoCapture(video_path)
 
